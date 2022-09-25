2b84da913d568d08492e7be5dfb8b2c9568c94a6
Denis Vabishchevich <Denis.Vabishchevich@gmail.com>
2022-09-25 07:09:06 +0000

    build automation

--- .space.kts
+++ .space.kts
@@ -4,6 +4,16 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job("Build and publish") {
+    container(displayName = "Build and notify", image = "gradle:7.1-jre11") {
+        kotlinScript { api ->
+            try {
+                api.gradle("build")
+            } catch (ex: Exception) {
+                val recipient = ChannelIdentifier.Channel(ChatChannel.FromName("CI-channel"))
+                val content = ChatMessage.Text("Build failed")
+                api.space().chats.messages.sendMessage(recipient, content)
+            }
+        }
+    }
 }
