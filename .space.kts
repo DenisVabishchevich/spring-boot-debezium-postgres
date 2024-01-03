f10e542f0c3e5af8dd10e6a685023326567cc7f9
DenisV <d.vabishchevich@uzum.com>
2024-01-03 17:09:54 +0300

    add devfile.yaml

--- .space.kts
+++ .space.kts
@@ -4,16 +4,10 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Build and publish") {
-    container(displayName = "Build and notify", image = "gradle:7.1-jre11") {
+job("Build and run tests") {
+    container(displayName = "Run gradle build", image = "amazoncorretto:17-alpine") {
         kotlinScript { api ->
-            try {
-                api.gradle("build")
-            } catch (ex: Exception) {
-                val recipient = ChannelIdentifier.Channel(ChatChannel.FromName("CI-channel"))
-                val content = ChatMessage.Text("Build failed")
-                api.space().chats.messages.sendMessage(recipient, content)
-            }
+            api.gradlew("build")
         }
     }
 }
